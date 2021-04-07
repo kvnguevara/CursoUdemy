ccff89d16652b16e2edaee2d53c7454b9e3c4326
kevin <kevinguevararacarrasco120@gmail.com>
2021-04-07 10:45:49 +0200

    Método que imprime cualquier Objeto (object object)

--- ArraysBidimensionles/src/test/TestMatrices.java
+++ ArraysBidimensionles/src/test/TestMatrices.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class TestMatrices {
     public static void main(String[] args) {
         //Creacion de la matriz
-        /*int edades [][] = new int[3][2];
+        int edades [][] = new int[3][2];
 
         Scanner sc = new Scanner(System.in);
         //para rellenarlo tenemos que hacerlo con dos for, una por fila y otro de columna
@@ -13,39 +13,51 @@ public class TestMatrices {
             for (int col =0; col < edades[fila].length; col++){ //para saber la longuitud de la columna
 
                 //Pedimos que rellene la matriz
-                System.out.println("Introduzca una edad :["+fila+"]["+col+"]");
-                edades[fila][col] = sc.nextInt();
+                edades[fila][col] = fila *25;
 
             }
         }
 
-        //Para mostrar la Matriz
+        // Para mostrar la Matriz
         for (int fila = 0; fila  < edades.length; fila ++){
             for (int col =0; col < edades[fila].length; col++){ //para saber la longuitud de la columna
                 //Mostramos lo que tiene la martiz
                 System.out.println("Edad :["+fila+"]["+col+"] ="+edades[fila][col] );
             }
-        } */
+        }
+
 
         //Creación y relleno de la matriz a la hora de crearlo
         String [][]frutas = {{"Naranja","Limon"},{"Fresa","Mora"}};
 
         //Mostramos la matriz de frtuas
-        for (int fila = 0; fila  < frutas.length; fila ++){
+        /*for (int fila = 0; fila  < frutas.length; fila ++){
             for (int col =0; col < frutas[fila].length; col++){ //para saber la longuitud de la columna
                 //Mostramos lo que tiene la martiz
                 System.out.print("{"+frutas[fila][col]+"}" );
             }
             System.out.println();
-        }
+        }*/
+
         
         //AHora con el for each
-        for (String[] f: frutas) {
+        /*for (String[] f: frutas) {
             for (String x:f) {
                 System.out.println("{"+x+"}");
             }
+        }*/
+        imprimir(frutas);
+
+
+    }
+
+    //Metodo para imprimir cualquier Objecto en Java
+    public static void imprimir(Object obj[][]){
+        //AHora con el for each
+        for (Object[] f: obj) {
+            for (Object x:f) {
+                System.out.println("{"+x+"}");
+            }
         }
-
-
     }
 }
--- ArraysBidimensionles/src/test/TestMatricesObject.java
+++ ArraysBidimensionles/src/test/TestMatricesObject.java
@@ -2,9 +2,29 @@ package test;
 
 import domain.Persona;
 
+import java.util.Scanner;
+
 public class TestMatricesObject {
     public static void main(String[] args) {
         //Creacion de las matrices de objetos
-        Persona personas[][]  = new Persona[1][1];
+        Persona personas[][]  = new Persona[2][3];
+        Scanner sc = new Scanner(System.in);
+        String nombre;
+
+        for(int  i = 0 ; i < personas.length; i++){
+
+            for(int j = 0; j < personas[i].length; j++){
+                personas[i][j]= new Persona();
+                System.out.println("Introduza nombre :["+ i +"]["+ j +"]");
+                personas[i][j].setNombre(sc.nextLine());
+
+            }
+        }
+
+        for (Persona[] n: personas) {
+            for (Persona p :n) {
+                System.out.println(p.toString());
+            }
+        }
     }
 }
